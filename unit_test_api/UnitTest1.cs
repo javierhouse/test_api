7fe23f5d5dd4b90a380f78e5060d7666183d4891
javierhouse <j.debenito@hotmail.com>
2024-02-25 17:12:41 +0100

    Init commit

---
+++ unit_test_api/UnitTest1.cs
@@ -0,0 +1,37 @@
+namespace unit_test_api;
+
+
+public class UnitTest1
+{
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public UnitTest1()
+    {
+        _logger = A.Fake<ILogger<WeatherForecastController>>();
+    }
+    
+    [Fact]
+    public void Test_Ends_Ko()
+    {
+        //Arrange
+        var WeatherForecast = A.Fake<ICollection<WeatherForecast>>();
+        WeatherForecastController controller = new WeatherForecastController(_logger);
+        
+        //Act
+        var result = controller.Get();
+        
+        //Assert
+        result.Should().NotBeNull();
+        
+    }
+
+/*
+    [Fact]
+    public void Test_Ends_Ok()
+    {
+        WeatherForecastController controller = new WeatherForecastController(_logger);
+        ObjectResult responseController = (ObjectResult)controller.GetPrueba (200);
+        Assert.Equal((int)HttpStatusCode.OK,responseController.StatusCode!.Value);
+    }
+    */
+}
---
+++ unit_test_api/Usings.cs
@@ -0,0 +1,10 @@
+global using Xunit;
+global using api.Controllers;
+global using api;
+//dotnet add package FluentAssertions
+global using FluentAssertions;
+//instalamos dotnet add package FakeItEasy 
+global using FakeItEasy;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.Extensions.Logging;
+global using System.Net;
